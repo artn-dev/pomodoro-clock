c19911d26fd170c9acabe6a70289dff26a962f97
artn-dev <azemarteixeira@gmail.com>
2021-06-18 15:29:07 -0300

    Add time counting logic

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ClockOptions from "./ClockOptions"
 import ClockDisplay from "./ClockDisplay"
 
 const Clock = () => {
-    const [time, setTime] = useState(25 * 60)
+    const [isRunning, setIsRunning] = useState(true)
+    const [time, setTime] = useState(5)
 
     const getMinutes = () => ( Math.floor(time / 60) )
     const getSeconds = () => ( time % 60 )
+
+    useEffect(() => {
+        if (isRunning && time > 0) {
+            setTimeout(() => { setTime(time - 1) }, 1000)
+        }
+    })
 
     return (
         <>
