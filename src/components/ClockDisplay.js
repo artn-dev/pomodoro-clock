70ce783986c2bfbd1a1a562d174c8c77966bb41f
artn-dev <azemarteixeira@gmail.com>
2021-06-18 15:20:44 -0300

    Set monospace font

--- src/components/ClockDisplay.js
+++ src/components/ClockDisplay.js
@@ -2,7 +2,7 @@ const ClockDisplay = ({ minutes, seconds }) => {
     return (
         <>
             <div className="bg-light border border-secondary rounded p-1 fs-1 mb-3 text-center w-100">
-                <p className="m-0">{minutes}:{seconds < 10 && "0"}{seconds}</p>
+                <p className="m-0 font-monospace">{minutes}:{seconds < 10 && "0"}{seconds}</p>
             </div>
         </>
     )
