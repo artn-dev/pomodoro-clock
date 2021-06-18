6235ae9cadf3bff25bd7c953409b80eacf8906a0
artn-dev <azemarteixeira@gmail.com>
2021-06-18 13:59:10 -0300

    Add clock buttons layout

---
+++ src/components/ClockOptions.js
@@ -0,0 +1,19 @@
+const ClockOptions = () => {
+  return (
+    <>
+      <div className="btn-group btn-group-lg" role="group" aria-label="Clock Options">
+        <button type="button" className="btn btn-danger">
+          Cancel
+        </button>
+        <button type="button" className="btn btn-secondary">
+          <i className="bi bi-gear"></i>
+        </button>
+        <button type="button" className="btn btn-success">
+          Start
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default ClockOptions
