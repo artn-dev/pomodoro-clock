7ecafc56eae93f93765af3e71bcc6d36963f93f0
artn-dev <azemarteixeira@gmail.com>
2021-06-18 15:07:02 -0300

    Add clock display layout

---
+++ src/components/ClockDisplay.js
@@ -0,0 +1,11 @@
+const ClockDisplay = ({ minutes, seconds }) => {
+    return (
+        <>
+            <div className="bg-light border border-secondary rounded p-1 fs-1 mb-3 text-center w-100">
+                <p className="m-0">{minutes}:{seconds < 10 && "0"}{seconds}</p>
+            </div>
+        </>
+    )
+}
+
+export default ClockDisplay
