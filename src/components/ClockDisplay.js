5628afcae5c470583e71610b5fa2d5d372fea91c
artn-dev <azemarteixeira@gmail.com>
2021-06-19 02:09:04 -0300

    Fix minute single digit display

--- src/components/ClockDisplay.js
+++ src/components/ClockDisplay.js
@@ -8,7 +8,9 @@ const ClockDisplay = () => {
     return (
         <>
             <div className="bg-light border border-secondary rounded p-1 fs-1 mb-3 text-center w-100">
-                <p className="m-0 font-monospace">{minutes}:{seconds < 10 && "0"}{seconds}</p>
+                <p className="m-0 font-monospace">
+                    {minutes < 10 && "0"}{minutes}:{seconds < 10 && "0"}{seconds}
+                </p>
             </div>
         </>
     )
