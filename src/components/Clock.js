3ae273cd7ca7c54a1b9924541281dc87dfe21eb3
artn-dev <azemarteixeira@gmail.com>
2021-06-19 03:29:02 -0300

    Update clock card style

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -7,8 +7,8 @@ import ConfigureTimeModal from './ConfigureTimeModal'
 const Clock = () => {
     return (
         <>
-            <div className="card d-flex align-items-center" style={{
-                maxWidth: 500,
+            <div className="card d-flex align-items-center border border-secondary border-4" style={{
+                maxWidth: 500, minWidth: 330,
             }}>
                 <div className="card-body w-75 py-5 d-flex flex-column align-items-center">
 
