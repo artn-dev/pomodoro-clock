c1697d0651b15090106c951e9cbeac0cc29556cc
artn-dev <azemarteixeira@gmail.com>
2021-06-19 01:56:33 -0300

    Add session-break cicle

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,10 +6,12 @@ import ClockDisplay from "./ClockDisplay"
 var countDownTimeout
 
 const Clock = () => {
-    const defaultTime = 5
+    const sessionTime = 5
+    const breakTime = 3
 
     const [isRunning, setIsRunning] = useState(false)
-    const [time, setTime] = useState(defaultTime)
+    const [sessionIsDone, setSessionIsDone] = useState(false)
+    const [time, setTime] = useState(sessionTime)
 
     const getMinutes = () => ( Math.floor(time / 60) )
     const getSeconds = () => ( time % 60 )
@@ -20,15 +22,32 @@ const Clock = () => {
 
     const resetTime = () => {
         clearTimeout(countDownTimeout)
-        setTime(defaultTime)
+        setTime(sessionTime)
         setIsRunning(false)
+        setSessionIsDone(false)
     }
 
     useEffect(() => {
-        if (isRunning && time > 0) {
+
+        if (!isRunning)
+            return
+
+        if (time > 0) {
             countDownTimeout = setTimeout(() => { setTime(time - 1) }, 1000)
+            return
+        } 
+
+        if (!sessionIsDone) {
+            setTimeout(() => {
+                setTime(breakTime)
+                setSessionIsDone(true)
+            }, 1000)
+            return
         }
-    }, [isRunning, time])
+
+        resetTime()
+
+    }, [isRunning, time, sessionIsDone])
 
     return (
         <>
