e19ca0d8f23eb1845b0cbde356b88e197bd70942
artn-dev <azemarteixeira@gmail.com>
2021-06-19 03:03:23 -0300

    Move slider to separate file

---
+++ src/components/TimeSlider.js
@@ -0,0 +1,38 @@
+const SessionTimeSlider = ({ minuteValue, onChangeMin, secondValue, onChangeSec }) => {
+    return (
+        <form>
+            <div className="d-flex">
+                <label htmlFor="sessionMinuteSlider" className="form-label">Minutes</label>
+                <p className="ms-auto border rounded px-2">
+                    {minuteValue < 10 && 0}{minuteValue}
+                </p>
+            </div>
+            <input
+                type="range"
+                className="form-range"
+                id="sessionMinuteSlider"
+                value={minuteValue}
+                onChange={(event) => {
+                    onChangeMin(parseInt(event.target.value))
+            }}/>
+
+            <div className="d-flex">
+                <label htmlFor="sessionSecondSlider" className="form-label">Seconds</label>
+                <p className="ms-auto border rounded px-2">
+                    {secondValue < 10 && 0}{secondValue}
+                </p>
+            </div>
+            <input
+                type="range"
+                className="form-range"
+                id="sessionSecondSlider"
+                value={secondValue}
+                onChange={(event) => {
+                    onChangeSec(parseInt(event.target.value))
+                }}
+            />
+        </form>
+    )
+}
+
+export default SessionTimeSlider 
