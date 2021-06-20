02fb4936dc9b3ec4454b313c15f735f1801938cb
artn-dev <azemarteixeira@gmail.com>
2021-06-19 22:00:01 -0300

    Add minimum width

--- src/components/ClockDisplay.js
+++ src/components/ClockDisplay.js
@@ -7,7 +7,11 @@ const ClockDisplay = () => {
 
     return (
         <>
-            <div className="bg-light border border-secondary rounded p-1 py-4 fs-1 mb-3 text-center w-100">
+            <div
+                className="bg-light border border-dark border-4 rounded p-1 py-4 fs-1 mb-3 text-center"
+                style={{
+                    minWidth: 300
+            }}>
                 <p className="m-0 font-monospace">
                     {minutes < 10 && "0"}{minutes}:{seconds < 10 && "0"}{seconds}
                 </p>
