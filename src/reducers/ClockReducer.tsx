891ddffb2a2542aec4774d0c252c771c9887ff9d
artn-dev <azemarteixeira@gmail.com>
2021-06-21 09:38:25 -0300

    Update ClockReducer

--- src/reducers/ClockReducer.tsx
+++ src/reducers/ClockReducer.tsx
@@ -1,5 +1,24 @@
-const ClockReducer = (state = {}, action) => {
-    return state
+export interface ClockReducerState {
+    currentTime: number
+    sessionTime: number
+    breakTime: number
+    isActive: boolean
+    sessionIsDone: boolean
+}
+
+const initialState = {
+    currentTime: 15 * 60,
+    sessionTime: 25 * 60,
+    breakTime: 5 * 60,
+    isActive: false,
+    sessionIsDone: false
+}
+
+const ClockReducer = (state: ClockReducerState = initialState, action) => {
+    switch (action.type) {
+    default:
+        return state
+    }
 }
 
 export default ClockReducer
