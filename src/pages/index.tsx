e64cfc8ded894edb1c7d69641229caf541d0e443
artn-dev <azemarteixeira@gmail.com>
2021-06-21 15:20:18 -0300

    Removed react-cookie component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
 import Clock from '../components/Clock'
-import { CookiesProvider } from 'react-cookie'
 
 
 function Home() {
   return (
     <>
-      <CookiesProvider>
-        <div className="container-fluid min-vh-100 bg-light d-flex" style={{
-          backgroundImage: "linear-gradient(100deg, var(--bs-primary), var(--bs-danger))"
-        }}>
-          <div className="mx-auto my-auto">
-            <Clock />
-          </div>
+      <div className="container-fluid min-vh-100 bg-light d-flex" style={{
+        backgroundImage: "linear-gradient(100deg, var(--bs-primary), var(--bs-danger))"
+      }}>
+        <div className="mx-auto my-auto">
+          <Clock />
         </div>
-      </CookiesProvider>
+      </div>
     </>
   )
 }
