b9e4fcb7586169bb5dcda751684606ecd04ed2c6
artn-dev <azemarteixeira@gmail.com>
2021-06-21 09:57:01 -0300

    Implement redux time update logic

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,9 +2,34 @@ import { TimerContextProvider } from '../contexts/TimerContext'
 import ClockOptions from "./ClockOptions"
 import ClockDisplay from "./ClockDisplay"
 import ConfigureTimeModal from './ConfigureTimeModal'
+import { useSelector, useDispatch } from 'react-redux'
+import { update } from '../actions/ClockActions'
+import { ClockReducerState } from '../reducers/ClockReducer'
+import { useEffect, useState } from 'react'
 
 
 const Clock = () => {
+    const isActive = useSelector((state: ClockReducerState) => (state.isActive))
+    const currentTime = useSelector((state: ClockReducerState) => (state.currentTime))
+    const dispatch = useDispatch()
+    const [clockTimeout, setClockTimeout] = useState<NodeJS.Timeout>()
+
+    const updateTime = () => {
+        setClockTimeout(
+            setTimeout(dispatch, 1000, update())
+        )
+    }
+
+    useEffect(() => {
+        if (!isActive) {
+            clearTimeout(clockTimeout)
+            return
+        }
+
+        updateTime()
+
+    }, [isActive, currentTime])
+
     return (
         <>
             <TimerContextProvider>
